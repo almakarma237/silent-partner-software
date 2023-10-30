eb717ddf601d4a0cdf320856daf155b2dbd5acbe
karma alma <almakarma237@gmail.com>
2023-10-30 21:02:40 +0100

    feat: updated folder structure

--- layouts/primary/PrimaryLayout.mocks.ts
+++
@@ -1,9 +0,0 @@
-import { IPrimaryLayout } from './PrimaryLayout';
-
-const base: IPrimaryLayout = {
-  sampleTextProp: 'Hello world!',
-};
-
-export const mockPrimaryLayoutProps = {
-  base,
-};
--- layouts/sidebar/SideBarLayout.mocks.ts
+++
@@ -1,9 +0,0 @@
-import { ISideBarLayout } from './SideBarLayout';
-
-const base: ISideBarLayout = {
-  sampleTextProp: 'Hello world!',
-};
-
-export const mockSideBarLayoutProps = {
-  base,
-};
